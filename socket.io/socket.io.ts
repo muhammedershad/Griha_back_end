aed1516defd409cc37b3bd358c3c9af853f36a25
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-01-25 21:40:41 +0530

    socket

--- socket.io/socket.io.ts
+++ socket.io/socket.io.ts
@@ -1,1 +1,41 @@
-import io from 'socket.io'
+import { Server } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+
+const videoCall = (io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
+    const emailToSocketIdMap = new Map();
+    const socketidToEmailMap = new Map();
+
+    io.on("connection", (socket) => {
+        console.log(`Socket Connected`, socket.id);
+
+        socket.emit('welcome','hello this socket server')
+
+        socket.on("room:join", (data) => {
+            const { email, room } = data;
+            emailToSocketIdMap.set(email, socket.id);
+            socketidToEmailMap.set(socket.id, email);
+            io.to(room).emit("user:joined", { email, id: socket.id });
+            socket.join(room);
+            io.to(socket.id).emit("room:join", data);
+        });
+
+        socket.on("user:call", ({ to, offer }) => {
+            console.log(to,'to', offer,'offer')
+            io.to(to).emit("incomming:call", { from: socket.id, offer });
+        });
+
+        socket.on('call:accepted', ({ to, ans }) => {
+            io.to(to).emit("call:accepted", { from: socket.id, ans });
+        })
+
+        socket.on('peer:nego:needed', ({ to, offer }) => {
+            io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+        })
+
+        socket.on('peer:nego:done', ({ to, ans }) => {
+            io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+        } )
+    });
+} 
+
+export default videoCall;
--- src/infrastructure/config/socket.io.ts
+++ src/infrastructure/config/socket.io.ts
@@ -1,5 +1,6 @@
 import http from "http";
 import { Server, Socket } from "socket.io";
+import videoCall from "../../../socket.io/socket.io";
 
 const configureSocket = (server: http.Server) => {
     const io = new Server(server, {
@@ -10,38 +11,7 @@ const configureSocket = (server: http.Server) => {
         maxHttpBufferSize: 5 * 1024 * 1024,
     });
 
-    const emailToSocketIdMap = new Map();
-    const socketidToEmailMap = new Map();
-
-    io.on("connection", (socket) => {
-        console.log(`Socket Connected`, socket.id);
-
-        socket.on("room:join", (data) => {
-            const { email, room } = data;
-            emailToSocketIdMap.set(email, socket.id);
-            socketidToEmailMap.set(socket.id, email);
-            io.to(room).emit("user:joined", { email, id: socket.id });
-            socket.join(room);
-            io.to(socket.id).emit("room:join", data);
-        });
-
-        socket.on("user:call", ({ to, offer }) => {
-            console.log(to,'to', offer,'offer')
-            io.to(to).emit("incomming:call", { from: socket.id, offer });
-        });
-
-        socket.on('call:accepted', ({ to, ans }) => {
-            io.to(to).emit("call:accepted", { from: socket.id, ans });
-        })
-
-        socket.on('peer:nego:needed', ({ to, offer }) => {
-            io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-        })
-
-        socket.on('peer:nego:done', ({ to, ans }) => {
-            io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-        } )
-    });
+    videoCall(io)
 
     return io;
 };
