95c9828c1764c1fc417e4ea5b44769b283884448
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-02-23 23:15:08 +0530

    ui changes

--- src/infrastructure/route/userRoute.ts
+++ src/infrastructure/route/userRoute.ts
@@ -26,15 +26,15 @@ router.post('/email', (req, res) => controller.email(req,res))
 router.get('/check-email', (req, res) => controller.checkEmail(req, res))
 router.get('/check-username', (req, res) => controller.checkUsername(req, res))
 router.post('/resend-otp', (req, res) => controller.resendOTP(req, res))
-router.get('/test', authMiddleware, (req, res) => controller.resendOTP(req, res))
-router.get('/users', ( req, res ) => controller.users( req, res ))
+// router.get('/test', authMiddleware, (req, res) => controller.resendOTP(req, res))
+router.get('/users', authMiddleware, ( req, res ) => controller.users( req, res ))
 router.patch('/block-user', adminAuthMiddleware, ( req, res ) => controller.blockUser( req, res ))
 router.patch( '/change-user-role', adminAuthMiddleware, ( req, res ) => controller.changeUserRole( req, res ))
 router.post( '/logout', ( req, res ) => controller.logout( req, res ))
 router.get('/all-clients', (req, res) => controller.allClients(req, res))
 router.post('/user', (req, res) => controller.userDetails(req, res))
 router.patch('/update-user-profile-photo', authMiddleware, (req, res) => controller.userProfilePicUpdate(req, res))
-router.get('/:userId', (req, res, next) => controller.user(req, res, next))
+router.get('/:userId',authMiddleware, (req, res, next) => controller.user(req, res, next))
 
 
 export { controller }
